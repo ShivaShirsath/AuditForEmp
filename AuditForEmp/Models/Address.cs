20557dbbf6020468c2695b79f5bcad0a571ae446
Imran Fattekhan <imran.fattekhan@p99soft.com>
2023-07-06 22:32:01 +0530

    Add project files.

---
+++ AuditForEmp/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace AuditForEmp.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
